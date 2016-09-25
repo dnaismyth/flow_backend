e3bd5a20c2d3922d6eea8fbd4a1c07bfe2513229
dnaismyth <naismythdayna@gmail.com>
2016-09-25 10:24:06 -0700

    Added signup request for creating a new user to hold basic required signup info

--- flow/src/main/java/com/movement/controller/SignupController.java
+++ flow/src/main/java/com/movement/controller/SignupController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.services.dynamodbv2.model.OperationType;
+import com.movement.controller.dto.RestResponse;
+import com.movement.controller.dto.SignupRequest;
+import com.movement.dto.Operation;
 import com.movement.dto.User;
 import com.movement.exception.ResourceNotFoundException;
 import com.movement.repository.UserRepository;
 import com.movement.service.UserService;
+import com.movement.util.RestPreconditions;
 
 /**
  * Controller used for the initial signup of a user
@@ -42,12 +47,24 @@ public class SignupController {
     
     /**
      * Create a user login
+     * Need to provide email, username, name, and password
      * @param user
      * @return
      */
     @RequestMapping(method = RequestMethod.POST)
-    public User createLogin(@RequestBody User user){
-    	User u = userService.create(user);
+    public RestResponse<User> createLogin(@RequestBody SignupRequest req){
+    	RestPreconditions.checkNotNull(req);
+    	User u = buildUser(req);
+    	return new RestResponse<User>(Operation.CREATE, u);
+    }
+    
+    // Build user from provided signup request
+    private User buildUser(SignupRequest req){
+    	User u = new User();
+    	u.setEmail(req.getEmail());
+    	u.setName(req.getName());
+    	u.setUsername(req.getUsername());
+    	u.setPassword(req.getPassword());
     	return u;
     }
 
---
+++ flow/src/main/java/com/movement/controller/dto/SignupRequest.java
@@ -0,0 +1,55 @@
+package com.movement.controller.dto;
+
+/**
+ * Request to store a new user's required fields
+ * on signing up for an account.
+ * @author DN
+ *
+ */
+public class SignupRequest {
+
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	
+	public SignupRequest(String username, String name, String password, String email){
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.name = name;
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+}
+
